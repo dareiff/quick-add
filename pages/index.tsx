1daede4edf0848d54e1acd0c02aff4d49b853a94
brainito <>
2024-10-10 23:37:30 -0700

    new icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -675,6 +675,11 @@ const Home: React.FC = () => {
     return (
         <AppContainer>
             <Head>
+                <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="Milk Money" />
                 <link rel="manifest" href="/manifest.json" />
                 <title>Milk Money</title>
             </Head>
