6e96a8a89b7481536186f118f99c2a15bab81086
brainito <18835952+brainito@users.noreply.github.com>
2024-12-23 23:45:36 -0600

    move settings to its own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,8 @@ import {
     HelpTooltip
 } from '../styles/styledComponents';
 
+import Settings from '../components/Settings';
+
 interface LunchMoneyCategory {
     id: number;
     name: string;
@@ -494,124 +496,28 @@ const Home: React.FC = () => {
                 </HeaderContainer>
 
                 {(!authenticated || settings) && (
-                    <div style={{ width: '100%', margin: '20px 0' }}>
-
-                        {accessTokenInState === null || accessTokenInState.length === 0 ? (
-                            <div>
-                                <h3>Welcome!</h3>
-                                <p>This app lets you quickly add manual transactions to LunchMoney on the go.</p>
-                                <p>To get started, add your access token (only stored locally on your device.)</p>
-                                <TinyField
-                                    placeholder='LunchMoney Access Token'
-                                    type='text'
-                                    ref={accessRef}
-                                />
-                                <TinyButton
-                                    onClick={() =>
-                                        addAccessTokenFromHTMLElement()
-                                    }
-                                >
-                                    Add
-                                </TinyButton>
-                            </div>
-                        ) : (
-                            <div>
-                                <h3>Settings:</h3>
-                                <div>
-                                    <TinyButton
-                                        onClick={() => {
-                                            localStorage.removeItem('access_token');
-                                            setAccessToken('');
-                                            setAuthenticated(false);
-                                        }}
-                                    >
-                                        Delete Access Token (...{accessTokenInState.slice(-6)})
-                                    </TinyButton>
-                                </div>
-                                <div>  {/* New toggle for quick buttons */}
-                                    <p></p><TinyButton onClick={() => {
-                                        setShowQuickButtons(!showQuickButtons);
-                                        showSettings(false);
-                                    }}>
-                                        {showQuickButtons ? 'Hide Quick Buttons' : 'Show Quick Buttons'}
-                                    </TinyButton>
-                                </div>
-                                {recentCategories.length !== 0 && (
-                                    <div>
-                                        <p></p><TinyButton
-                                            onClick={() => {
-                                                localStorage.removeItem('recentCategories');
-                                                setRecentCategories([]);
-                                                showSettings(false);
-                                            }}
-                                        >
-                                            Clear Recent Categories
-                                        </TinyButton>
-                                    </div>
-                                )}
-                                {recentAssets.length !== 0 && (
-                                    <div>
-                                        <p></p><TinyButton
-                                            onClick={() => {
-                                                localStorage.removeItem('recentAssets');
-                                                setRecentAssets([]);
-                                                showSettings(false);
-                                            }}
-                                        >
-                                            Clear Recent Assets
-                                        </TinyButton>
-                                    </div>
-                                )}
-                                <div>
-                                    <p></p><label htmlFor="recentCount">Recent categories to show:</label>
-                                    <SelectContainer>
-                                        <StyledSelect
-                                            id="recentCount"
-                                            value={recentCount}
-                                            onChange={handleRecentCountChange}
-                                        >
-                                            {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
-                                                <option key={num} value={num}>
-                                                    {num}
-                                                </option>
-                                            ))}
-                                        </StyledSelect>
-                                    </SelectContainer>
-                                </div>
-                                <div>
-                                    <p></p><label htmlFor="tags">Tag all transactions as:</label>
-                                    <Popover placement="top-start">
-                                        <PopoverTrigger>
-                                            <HelpIcon>?</HelpIcon>
-                                        </PopoverTrigger>
-                                        <PopoverContent>
-                                            <HelpTooltip>
-                                                Tag(s) that will be added to all transactions (e.g., &quot;cash&quot;,
-                                                &quot; manual-cash&quot;, &quot;milk-money&quot;, etc.) to make it easier
-                                                to track transactions entered from this tool.
-                                                <p></p>Leave empty to disable.
-                                            </HelpTooltip>
-                                        </PopoverContent>
-                                    </Popover>
-                                    <CreatableSelect
-                                        id="tags"
-                                        components={components}
-                                        inputValue={inputValue}
-                                        isClearable
-                                        isMulti
-                                        menuIsOpen={false}
-                                        onChange={(newValue) => {setTags(newValue)}}
-                                        onInputChange={(newValue: any) => setInputValue(newValue as string)}
-                                        onKeyDown={handleKeyDown}
-                                        onBlur={handleBlur}
-                                        value={tags}
-                                        styles={menuStyles}
-                                        placeholder= "Type to set tag(s)..."
-                                    />
-                                </div>
-                            </div>
-                        )}
-                    </div>
+                    <Settings
+                        accessTokenInState={accessTokenInState}
+                        setAccessToken={setAccessToken}
+                        setAuthenticated={setAuthenticated}
+                        showSettings={showSettings}
+                        showQuickButtons={showQuickButtons}
+                        setShowQuickButtons={setShowQuickButtons}
+                        recentCategories={recentCategories}
+                        setRecentCategories={setRecentCategories}
+                        recentAssets={recentAssets}
+                        setRecentAssets={setRecentAssets}
+                        recentCount={recentCount}
+                        setRecentCount={setRecentCount}
+                        tags={tags}
+                        setTags={setTags}
+                        inputValue={inputValue}
+                        setInputValue={setInputValue}
+                        handleKeyDown={handleKeyDown}
+                        handleBlur={handleBlur}
+                        addAccessTokenFromHTMLElement={addAccessTokenFromHTMLElement}
+                        accessRef={accessRef}
+                    />
                 )}
 
                 {authenticated && !settings && (
